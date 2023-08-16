2d82f656eeeda0deb355fd2b62503a9585e5e918
Krishnapal Zala <109763124+Krish-Na-Pal@users.noreply.github.com>
2023-08-17 01:18:30 +0530

    Update settings.py

--- mingleVision/mingleVision/settings.py
+++ mingleVision/mingleVision/settings.py
@@ -132,4 +132,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'minglevision1508@gmail.com'
-EMAIL_HOST_PASSWORD = 'wlfkeretitnspdhq'
+EMAIL_HOST_PASSWORD = '<YOUR EMAIL PASS KEY>'
